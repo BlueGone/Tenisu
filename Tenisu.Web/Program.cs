30ae9f1af3680c1c881a0fe9ebd93454abbdd691
Thomas Plaçais <thomas.placais@protonmail.com>
2026-02-07 13:23:30 +0100

    feat: add OpenAPI generation with Scalar UI

--- Tenisu.Web/Program.cs
+++ Tenisu.Web/Program.cs
@@ -1,7 +1,15 @@
+using Scalar.AspNetCore;
 using Tenisu.Web.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+
 var app = builder.Build();
+
+app.MapGet("/", () => Results.Redirect("/scalar")).ExcludeFromDescription();
+app.MapOpenApi();
+app.MapScalarApiReference();
 
 app.MapPlayerEndpoints();
 
